55168351ec4a9e6e8556212f9d742e41ec1adbf7
Andres Prieto <afprietoa@unal.edu.co>
2021-12-20 12:13:31 -0500

    Conection with Bakend-Heroku

--- src/context/SocketContext.js
+++ src/context/SocketContext.js
@@ -7,7 +7,7 @@ export const SocketContext = createContext();
 
 export const SocketProvider = ({ children }) => {
 
-    const { socket, online } = useSocket('http://localhost:8080');
+    const { socket, online } = useSocket('https://socket-server-wabike1.herokuapp.com/');
     
     return (
         <SocketContext.Provider value={{ socket, online }}>
